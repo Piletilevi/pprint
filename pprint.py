8c1c0e1737e0213252a91b89d4f01ac6995e5c67
michelek <mihkel.putrinsh@gmail.com>
2018-04-24 08:06:12 +0300

    printingDriverVersion

--- pprint.py
+++ pprint.py
@@ -22,27 +22,15 @@ global PLP_JSON_DATA
 PLP_FILENAME = sys.argv[1]
 with open(PLP_FILENAME, 'r', encoding='utf-8') as plp_data_file:
     PLP_JSON_DATA = json.load(plp_data_file)
-if PLP_JSON_DATA.get('plpVersion') != __version__:
+
+# 0
+# Update
+# Make sure we are on required version
+if PLP_JSON_DATA.get('printingDriverVersion') != __version__:
     from _update import update
-    print('updating from ' + __version__ + ' to ' + PLP_JSON_DATA.get('plpVersion'))
+    print('updating from ' + __version__ + ' to ' + PLP_JSON_DATA.get('printingDriverVersion'))
     update('https://github.com/Piletilevi/printsrv3/raw/8a19e3531a91d77dfa14f51425e6b9ed3bc98df5/plevi.zip')
 
 
-global FEEDBACK_TEMPLATE
-# with open(os.path.join(BASEDIR, 'package.json'), 'r') as package_json_file:
-#     global PACKAGE_JSON_DATA
-#     PACKAGE_JSON_DATA = json.load(package_json_file)
-
-fbtmpl_fn = os.path.join(BASEDIR, 'config', 'feedbackTemplate.json')
-with open(fbtmpl_fn, 'r', encoding='utf-8') as feedback_template_file:
-    FEEDBACK_TEMPLATE = json.load(feedback_template_file)
-    FEEDBACK_TEMPLATE['feedbackToken'] = PLP_JSON_DATA.get('feedbackToken')
-    FEEDBACK_TEMPLATE['operationToken'] = PLP_JSON_DATA\
-        .get('operationToken')
-    FEEDBACK_TEMPLATE['businessTransactionId'] = PLP_JSON_DATA\
-        .get('fiscalData', {'businessTransactionId': ''})\
-        .get('businessTransactionId', '')
-    FEEDBACK_TEMPLATE['operation'] = PLP_JSON_DATA\
-        .get('fiscalData').get('operation')
 
 print('Initialized', __version__)
