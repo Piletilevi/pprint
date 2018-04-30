87818b7adb76c540a60be84813fc2c6dcbcb28fd
michelek <mihkel.putrinsh@gmail.com>
2018-04-30 09:09:20 +0300

    move in feedback code

--- decorators.py
+++ decorators.py
@@ -37,6 +37,74 @@ class profiler(object):
         return wrapped_f
 
 
+class feedback():
+
+    def __init__(self, f):
+        # print(f.__dir__())
+        f.bye = bye
+        f.feedback = feedback
+
+    def bye(title = ''):
+        # input("Press Enter to continue...")
+        if title:
+            import ctypes
+            (a,b,c) = sys.exc_info()
+            if b:
+                ctypes.windll.user32.MessageBoxW(0, "{0}\n-----\n{1}".format(title,b), title, 0)
+                # ctypes.windll.user32.MessageBoxW(0, "{0}\n{1}\n{2}".format(a,b,c), title, 0)
+            else:
+                ctypes.windll.user32.MessageBoxW(0, "{0}\n-----\nExiting".format(title), title, 0)
+        kill(getpid(), signal.SIGTERM)
+
+    fbtmpl_fn = path.join(BASEDIR, 'config', 'feedbackTemplate.json')
+    with open(fbtmpl_fn, 'r', encoding='utf-8') as feedback_template_file:
+        FEEDBACK_TEMPLATE = loadJSON(feedback_template_file)
+        FEEDBACK_TEMPLATE['feedbackToken'] = PLP_JSON_DATA.get('feedbackToken')
+        FEEDBACK_TEMPLATE['operationToken'] = PLP_JSON_DATA.get('operationToken')
+        FEEDBACK_TEMPLATE['businessTransactionId'] = PLP_JSON_DATA.get('fiscalData', {'businessTransactionId':''}).get('businessTransactionId', '')
+        FEEDBACK_TEMPLATE['operation'] = PLP_JSON_DATA.get('fiscalData').get('operation')
+
+
+    fblog_fn = path.join(BASEDIR, 'feedback.log')
+    def fb2log(line):
+        with open(fblog_fn, 'a', encoding='utf-8') as feedback_log_file:
+            feedback_log_file.write(datetime.now().isoformat() + ' ' + str(line) + '\n')
+
+
+    def feedback(feedback, success=True, reverse=None):
+        FEEDBACK_TEMPLATE['status'] = success
+        FEEDBACK_TEMPLATE['feedBackMessage'] = feedback.get('message')
+
+        _fburl = PLP_JSON_DATA.get('feedbackUrl', PLP_JSON_DATA.get('feedBackurl'))
+        # print('Sending "{0}" to "{1}"'.format(dumpsJSON(FEEDBACK_TEMPLATE, indent=4), _fburl))
+        fb2log('Sending ' + feedback.get('message'))
+        headers = {'Content-type': 'application/json'}
+        r = requests.post(_fburl, allow_redirects=True, timeout=30, json=FEEDBACK_TEMPLATE, verify=False)
+        fb2log('\\_ sent ok.')
+
+        if r.status_code != requests.codes.ok:
+            fb2log('Not Ok - (' + r.status_code + ')')
+            if reverse:
+                reverse()
+            bye('{0}; status_code={1}'.format(r.headers['content-type'], r.status_code))
+
+        try:
+            response_json = r.json()
+            # print('BO response: {0}'.format(dumpsJSON(response_json, indent=4)))
+        except Exception as e:
+            fb2log('Not Ok - Feedback failed, reversing operation')
+            import ctypes
+            ctypes.windll.user32.MessageBoxW(0, "Feedback failed", "Reversing operation", 0)
+            # print(e)
+            # print('BO response: {0}'.format(r.text))
+            if reverse:
+                reverse()
+            bye()
+
+        fb2log('Ok')
+
+
+
 class logging(object):
     def __init__(self, f):
         # print(f.__dir__())
