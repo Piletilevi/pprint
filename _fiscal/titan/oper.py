8c8efaa328b72be6d78ef4930c3ec0c9af97a135
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-05-10 11:22:06 +0300

    write table test

--- _fiscal/titan/oper.py
+++ _fiscal/titan/oper.py
@@ -1,27 +1,5 @@
 # This Python file uses the following encoding: utf-8
 
-# // 20180506102118
-# // http://169.254.186.173/cgi/tbl/Oper
-#
-# [
-#   { "id": 1, "Name": "OP1", "Pswd": 1 },
-#   { "id": 2, "Name": "OP2", "Pswd": 22 },
-#   { "id": 3, "Name": "ОПЕРАТОР 3", "Pswd": 3 },
-#   { "id": 4, "Name": "ОПЕРАТОР 4", "Pswd": 4 },
-#   { "id": 5, "Name": "ОПЕРАТОР 5", "Pswd": 5 },
-#   { "id": 6, "Name": "ОПЕРАТОР 6", "Pswd": 6 },
-#   { "id": 7, "Name": "ОПЕРАТОР 7", "Pswd": 7 },
-#   { "id": 8, "Name": "ОПЕРАТОР 8", "Pswd": 8 },
-#   { "id": 9, "Name": "ОПЕРАТОР 9", "Pswd": 9 },
-#   { "id": 10, "Name": "ОПЕРАТОР 10", "Pswd": 10 },
-#   { "id": 11, "Name": "ОПЕРАТОР 11", "Pswd": 11 },
-#   { "id": 12, "Name": "ОПЕРАТОР 12", "Pswd": 12 },
-#   { "id": 13, "Name": "ОПЕРАТОР 13", "Pswd": 13 },
-#   { "id": 14, "Name": "ОПЕРАТОР 14", "Pswd": 14 },
-#   { "id": 15, "Name": "ОПЕРАТОР 15", "Pswd": 15 },
-#   { "id": 16, "Name": "mmp", "Pswd": 123 }
-# ]
-
 
 import sys
 import requests
@@ -48,14 +26,15 @@ def talk(url, method='GET', data={}):
         r = requests.post(url, auth=auth, json=data, headers=headers)
 
     print('S:"{}"; R:"{}"'.format(r.status_code, r.reason))
-    print(r.text.encode('cp1251'))
-    ro = {
-        'status_code': r.status_code,
-        'reason': r.reason,
-        'body': json.loads(r.text, 'cp1251')
-    }
-    # print('S:"{}"; R:"{}"; B:"{}"'.format(r.status_code, r.reason, body))
-    return ro
+    return r
+
+
+# SetTime
+def setTime():
+    ISOdatetime = datetime.datetime.fromtimestamp(1463288494).isoformat()
+    url = 'http://169.254.186.173/cgi/proc/setclock?{datetime}' \
+        .format(datetime=ISOdatetime)
+    talk(url)
 
 
 # Beep
@@ -65,19 +44,96 @@ def beep(ms=300, f=660):
 
 
 # State
-def fiskalMode():
+def getFiskalMode():
     url = 'http://169.254.186.173/cgi/state'
     r = talk(url)
-    return r['body']['FskMode']
+    return json.loads(r.text, 'cp1251')['FskMode']
 
 
-# Flg feed 1
+# whiteIP
+def whiteIP():
+    url = 'http://169.254.186.173/cgi/proc/register?clear'
+    talk(url)
+    # print(json.dumps(readTable('whiteIP'), indent=4))
+    url = 'http://169.254.186.173/cgi/proc/register'
+    talk(url)
+    # print(json.dumps(readTable('whiteIP'), indent=4))
+
+
+# Tables
+def tables():
+    url = 'http://169.254.186.173/cgi/tbl'
+    r = talk(url)
+
+    jbody = json.loads(r.text[3:])
+    return jbody
+
+
+# read table
+def readTable(name):
+    url = 'http://169.254.186.173/cgi/tbl/{name}'.format(name=name)
+    r = talk(url)
+    jbody = json.loads(r.text)
+    return jbody
+
+
+# read table
+def writeTable(name, data):
+    url = 'http://169.254.186.173/cgi/tbl/{name}'.format(name=name)
+    r = talk(url, 'PATCH', data)
+    jbody = json.loads(r.text)
+    return jbody
+
+
+# cgi/chk
+def printBill(data):
+    url = 'http://169.254.186.173/cgi/chk'
+    r = talk(url, 'POST', data)
+    return r
+
+
+# Set feed
 def setFeed(lines):
-    url = 'http://169.254.186.173/cgi/tbl/Flg'
-    talk(url)
-    data = {'Feed': lines}
-    talk(url, 'PATCH', data)
-    talk(url)
+    print(json.dumps(readTable('Flg'), indent=4))
+
+    data = []
+    data.append({'Feed': lines})
+    print(json.dumps(writeTable('Flg', 'data'), indent=2))
+
+    print(json.dumps(readTable('Flg'), indent=4))
+
+
+#
+# main
+#
+whiteIP()
+print('Fiscal mode: {}'.format(getFiskalMode()))
+# print(json.dumps(readTable('PLU'), indent=4))
+
+# setFeed(4)
+url = 'http://169.254.186.173/cgi/tbl/Flg'
+data = {'Feed': 2}
+r = talk(url, 'PATCH', data)
+print(json.dumps(readTable('Flg'), indent=4))
+
+jbill = json.loads('''{"F":[
+    {"C":{"cm":"Кассир: Светлана"}},
+    {"S":{"code":"1","price":"5","name":"Конфета"}},
+    {"S":{"code":"2","price":"15","name":"Печенье", "qty":"0.5"}},
+    {"D":{ "prc":"5", "all":"1"}},
+    {"P":{}}
+    ]}''')
+
+# r = printBill(jbill)
+# print(r.text)
+
+
+# print(json.dumps(tables(), indent=4))
+# print(json.dumps(readTable('whiteIP'), indent=4))
+# print(json.dumps(readTable('TCP'), indent=4))
+# print(json.dumps(readTable('Host'), indent=4))
+# print(json.dumps(readTable('Dir'), indent=4))
+# # # print(json.dumps(readTable('SysLog'), indent=4))
 
 
 # Oper
@@ -90,26 +146,17 @@ def setOperator(name, pswd):
     talk(url)
 
 
-# whiteIP
-def whiteIP():
-    url = 'http://169.254.186.173/cgi/proc/register?clear'
-    talk(url)
-    url = 'http://169.254.186.173/cgi/proc/register'
-    talk(url)
-    # talk(url)
-    url = 'http://169.254.186.173/cgi/tbl/whiteIP'
-    talk(url)
-
-
-beep(40, 2000)
-
 sys.exit(0)
-# SetTime
-ISOdatetime = datetime.datetime.fromtimestamp(1463288494).isoformat()
-url = 'http://169.254.186.173/cgi/proc/setclock?{datetime}'.format(datetime=ISOdatetime)
-talk(url)
 
 
 #
 # url = 'http://169.254.186.173/cgi/proc/fiscalization'
 # talk(url)
+
+# Do not play this sequence too often
+beep(80,  262)  # C4
+beep(80,  330)  # E4
+beep(80,  392)  # G4
+beep(160, 523)  # C5
+beep(80,  392)  # G4
+beep(480, 523)  # C5
